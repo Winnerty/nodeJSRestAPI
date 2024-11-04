aa6d79df269cccec381eceebe31d181dfb849673
Winnerty <timofey.n.karpeev@gmail.com>
2024-11-04 17:16:59 +0100

    dd

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const usersRouter = require("./routes/users.js")
+const sqlite3 = require("sqlite3").verbose()
 
-//MIDDLEWARE
-app.use(express.json())
-//users endpoints
-app.use("/api/", usersRouter)
+// Open the database connection
+const db = new sqlite3.Database("./users.db", (err) => {
+	if (err) {
+		console.error("Error opening database:", err.message)
+	} else {
+		console.log("Connected to the SQLite database.")
 
-//HOME GET METHOD
-app.get("/", (req, res) => {
-    res.json(usersArray)
+		// Create the items table if it doesn't exist
+		db.run(
+			`CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        firstName TEXT NOT NULL,
+        lastName TEXT NOT NULL
+      )`,
+			(err) => {
+				if (err) {
+					console.error("Error creating table:", err.message)
+				}
+			}
+		)
+	}
 })
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`)
-})
+module.exports = db
